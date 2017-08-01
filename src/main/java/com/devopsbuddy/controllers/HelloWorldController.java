076af4ecae8cc2aa4a4021eb75970fc848e6d773
Kensaigm <scarroll10@student.cccd.edu>
2017-08-01 15:27:58 -0700

    updated HelloWorldController to not conflict with default controller.

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorldController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/hw/")
 	public String sayHello() {
-		return "index";
+		return "index2";
 	}
 
 }
